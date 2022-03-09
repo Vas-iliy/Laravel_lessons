036d7c93e4dbb714fdb0cf7e04d0e9ed02bbaf83
Vasiliy <kkolyasev1228@gmail.com>
2022-03-09 18:06:03 +0400

    db crud

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
 class HomeController extends Controller
 {
     public function index()
     {
+        //DB::insert("INSERT INTO posts (title, content) VALUES (?, ?)", ['state5', 'content state 5']);
+        //DB::update("UPDATE posts SET created_at = ?, updated_at = ? WHERE id = ?", [NOW(), NOW(), 1]);
+        //DB::delete("DELETE FROM posts WHERE id = ?", [4]);
+
+        $posts = DB::select("SELECT * FROM posts WHERE id > ?", [3]);
+        dd($posts);
         return view('home');
     }
 

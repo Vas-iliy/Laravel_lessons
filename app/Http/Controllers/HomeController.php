6f5bf525e01051c2a34cb4bb312056d7b271a935
Vasiliy <kkolyasev1228@gmail.com>
2022-03-09 19:02:07 +0400

    table

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,12 +8,39 @@ class HomeController extends Controller
 {
     public function index()
     {
-        //DB::insert("INSERT INTO posts (title, content) VALUES (?, ?)", ['state5', 'content state 5']);
-        //DB::update("UPDATE posts SET created_at = ?, updated_at = ? WHERE id = ?", [NOW(), NOW(), 1]);
-        //DB::delete("DELETE FROM posts WHERE id = ?", [4]);
-
+        // db
+        /*DB::insert("INSERT INTO posts (title, content) VALUES (?, ?)", ['state5', 'content state 5']);
+        DB::update("UPDATE posts SET created_at = ?, updated_at = ? WHERE id = ?", [NOW(), NOW(), 1]);
+        DB::delete("DELETE FROM posts WHERE id = ?", [4]);
         $posts = DB::select("SELECT * FROM posts WHERE id > ?", [3]);
-        dd($posts);
+        dd($posts);*/
+//table
+/*        $data = DB::table('country')->get();
+        $data = DB::table('country')->limit(5)->get();
+        $data = DB::table('country')->select('Code', 'Name')->limit(5)->get();
+        $data = DB::table('country')->select('Code', 'Name')->first();
+        $data = DB::table('country')->select('Code', 'Name')->orderBy('Code', 'desc')->first();
+        $data = DB::table('city')->select('ID', 'Name')->find(2);
+        $data = DB::table('city')->select('ID', 'Name')->where('id', '<',  5)->get();
+        $data = DB::table('city')->select('ID', 'Name')->where([
+            ['id', '>', 1],
+            ['id', '<', 5]
+        ])->get();
+        $data = DB::table('city')->where('id', '<', 3)->value('Name');
+        $data = DB::table('country')->limit(10)->pluck('Name', 'Code');
+
+        $data = DB::table('country')->count();
+        $data = DB::table('country')->max('population');
+        $data = DB::table('country')->min('population');
+        $data = DB::table('country')->sum('population');
+        $data = DB::table('country')->avg('population');
+        $data = DB::table('city')->select('CountryCode')->distinct()->get();
+
+        $data = DB::table('city')->select('city.ID', 'city.Name as city_name',
+        'country.Code', 'country.Name as county_name')->limit(10)->join('country',
+            'city.CountryCode', '=', 'country.Code')->orderBy('city.ID')->get();
+        dd($data);*/
+
         return view('home');
     }
 
